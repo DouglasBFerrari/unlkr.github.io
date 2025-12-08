d36c11399785d7a8bf18baa53fc2ff320aadea89
Unlkr <freakduggy@gmail.com>
2025-12-08 20:39:11 -0300

    step one

--- assets/js/header.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    fetch("header.html") // Busca o arquivo header.html
-        .then(response => response.text()) // Converte para texto
-        .then(data => {
-            document.getElementById("header").innerHTML = data; // Insere o conteúdo no elemento div#header
-        })
-        .catch(error => console.error("Erro ao carregar o header:", error));
-});
